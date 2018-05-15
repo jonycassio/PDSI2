07147916bd4f2ae7b44e0b31c66797610d42460e
Renesio <38991110+Renesio@users.noreply.github.com>
2018-05-15 12:38:01 -0300

    implementacao das funcionalidades e acoes dos botoes da funcionalidades cadastro de massas fazendo o armazenamento dos dados no banco de dados

--- Projeto_PDSI_II/src/Cadastro_Alimenticio/Cadastro_Alimentos_Massas.java
+++ Projeto_PDSI_II/src/Cadastro_Alimenticio/Cadastro_Alimentos_Massas.java
@@ -6,28 +6,34 @@ import Banco_de_Dados.DAO;
 import Botoes.Borda_Redonda;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import projeto_pdsi_ii.Massas;
 
 
-public class Cadastro_Alimentos_Massas extends JFrame {
+public class Cadastro_Alimentos_Massas extends JFrame implements ActionListener {
     
-    
+    Massas dados_M = new Massas();
     JButton Voltar = new JButton("Voltar");
     JButton Adicionar = new JButton("Cadastrar");
  
 
     JTextField Pega_Nome_Fornecedor = new JTextField();
-    JTextField Pega_UnidadesKG = new JTextField();
-    JTextField Pega_QuantidadeKG = new JTextField();
+    JTextField Pega_Quantidade = new JTextField();
     JTextField Pega_Unidade_porcao = new JTextField();
 
     
@@ -44,59 +50,61 @@ public class Cadastro_Alimentos_Massas extends JFrame {
 
            
         JLabel Massas = new JLabel("Massas: ");
-        Pega_Massas.setBounds(250, 165, 130, 30);
-        Massas.setBounds(180, 160, 130, 40);
+        Pega_Massas.setBounds(250, 185, 130, 30);
+        Massas.setBounds(180, 180, 130, 40);
         Pega_Massas.setFont(fonte);
         Massas.setFont(fonte);
         add(Pega_Massas);
         add(Massas);       
         
         Pega_Massas.addItem("");
-        
+        Pega_Massas.addItem("pão hamburguer");
+        Pega_Massas.addItem("pão sanduichi");
+        Pega_Massas.addItem("pão hamburguer");
+      
             
         
         
         JLabel Fornecedor = new JLabel("Fornecedor:");
-        Pega_Nome_Fornecedor.setBounds(270, 245, 210, 30);
-        Fornecedor.setBounds(180, 240, 130, 40);
+        Pega_Nome_Fornecedor.setBounds(270, 265, 210, 30);
+        Fornecedor.setBounds(180, 260, 130, 40);
         Pega_Nome_Fornecedor.setFont(fonte);
         Fornecedor.setFont(fonte);
         add(Pega_Nome_Fornecedor);
         add(Fornecedor);
+                
         
-        
-                
-                
-        JLabel Quantidade = new JLabel("Quantidade:");
-        Pega_QuantidadeKG.setBounds(740, 165, 100, 30);
-        Quantidade.setBounds(650, 160, 130, 40);
-        Pega_QuantidadeKG.setFont(fonte);
-        Quantidade.setFont(fonte);
-        add(Pega_QuantidadeKG);
-        add(Quantidade);
-        
-                
-                
-        JLabel UP = new JLabel("Unidades por porção:");
-        Pega_Unidade_porcao.setBounds(805, 225, 100, 30);
-        UP.setBounds(650, 220, 160, 40);
-        Pega_Unidade_porcao.setFont(fonte);
+        JLabel UP = new JLabel("Unidades:");
+        Pega_Quantidade.setBounds(695, 185, 100, 30);
+        UP.setBounds(620, 180, 160, 40);
+        Pega_Quantidade.setFont(fonte);
         UP.setFont(fonte);
-        add(Pega_Unidade_porcao);
+        add(Pega_Quantidade);
         add(UP);
+        
+        
+        
+        
+        JLabel UP1 = new JLabel("Uni. por porção:");
+        Pega_Unidade_porcao.setBounds(730, 265, 100, 30);
+        UP1.setBounds(620, 260, 160, 40);
+        Pega_Unidade_porcao.setFont(fonte);
+        UP1.setFont(fonte);
+        add(Pega_Unidade_porcao);
+        add(UP1);
         
         
         
         Voltar.setBorder(new Borda_Redonda(7));
         Voltar.setBounds(130, 430, 160, 40);
-        //Adicionar.addActionListener(this);
+        Voltar.addActionListener(this);
         Voltar.setFont(fonte);
         add(Voltar);
         
         
         Adicionar.setBorder(new Borda_Redonda(7));
         Adicionar.setBounds(808, 430, 160, 40);
-        //Adicionar.addActionListener(this);
+        Adicionar.addActionListener(this);
         Adicionar.setFont(fonte);
         add(Adicionar);
 
@@ -114,6 +122,75 @@ public class Cadastro_Alimentos_Massas extends JFrame {
     
     }
     
+     public void preenche(){
+    
+            
+            dados_M.setTipo((String) Pega_Massas.getSelectedItem());
+            dados_M.setFornecedor(Pega_Nome_Fornecedor.getText());
+            dados_M.setQuantidade(Integer.parseInt(Pega_Quantidade.getText()));
+            dados_M.setQuant_porcao(Integer.parseInt(Pega_Unidade_porcao.getText()));
+         
+    }
+    public void actionPerformed(ActionEvent e) {
+
+      
+       
+        if (e.getSource() == Adicionar) {
+            
+            try {
+                 preenche();
+                ArmazenaDados();
+                
+                
+            } catch (SQLException ex) {
+                
+        
+
+            }
+        
+        }
+
+        else if (e.getSource() == Voltar) {
+            
+            dispose();
+           
+        }
+    }
+ 
+    public void ArmazenaDados() throws SQLException {
+
+                
+         
+        
+         c.conexao();
+        String sql = "insert into cad_massas (Tipo, Fornecedor,Quantidade, Quantidade_porcao) values (?,?,?,?);";
+
+        PreparedStatement stmt = c.conn.prepareStatement(sql);
+        
+        
+        try {
+                
+            stmt.setString(1,dados_M.getTipo());
+            stmt.setString(2,dados_M.getFornecedor());
+            stmt.setFloat(3,dados_M.getQuantidade());
+            stmt.setFloat(4,dados_M.getQuant_porcao());
+         
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Não foi possivel cadastrar este produto!");
+                        
+            Logger.getLogger(Cadastro_de_Bebidas.class.getName()).log(Level.SEVERE, null, ex);
+                       
+       } 
+   
+        
+        stmt.execute();
+
+        stmt.close();
+        
+    }
+
+    
     
     
     public static void main(String [] args){
@@ -121,7 +198,8 @@ public class Cadastro_Alimentos_Massas extends JFrame {
         new Cadastro_Alimentos_Massas();
         
     }
-    
+
+ 
     
     
 }
