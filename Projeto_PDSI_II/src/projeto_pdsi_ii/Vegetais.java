8f6189e5ef92568e523680fbf1993041a2c1bd58
Renesio <38991110+Renesio@users.noreply.github.com>
2018-05-17 16:36:52 -0300

    criação dos metodos geteres e seteres para cadastro dos vegetais

---
+++ Projeto_PDSI_II/src/projeto_pdsi_ii/Vegetais.java
@@ -0,0 +1,57 @@
+package projeto_pdsi_ii;
+
+public class Vegetais {
+
+  private String Tipo;
+    private String Fornecedor;
+    private float QuantTKG;
+    private float Precokg;
+    private float qunTPorC;
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getFornecedor() {
+        return Fornecedor;
+    }
+
+    public void setFornecedor(String Fornecedor) {
+        this.Fornecedor = Fornecedor;
+    }
+
+    public float getQuantTKG() {
+        return QuantTKG;
+    }
+
+    public void setQuantTKG(float QuantTKG) {
+        this.QuantTKG = QuantTKG;
+    }
+
+    public float getPrecokg() {
+        return Precokg;
+    }
+
+    public void setPrecokg(float Precokg) {
+        this.Precokg = Precokg;
+    }
+
+    public float getQunTPorC() {
+        return qunTPorC;
+    }
+
+    public void setQunTPorC(float qunTPorC) {
+        this.qunTPorC = qunTPorC;
+    }
+    
+
+public Vegetais() {
+        
+        
+    }
+
+}
